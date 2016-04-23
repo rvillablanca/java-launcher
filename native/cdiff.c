4661767132b04d0eaecc68143a2b5b84ff356772
Rodrigo Villablanca <villa061004@gmail.com>
2016-04-22 23:44:32 -0300

    Avances

--- native/cdiff.c
+++ native/cdiff.c
@@ -7,29 +7,50 @@
 #include <ctype.h>
 
 struct s_props {
+    //    Required
     int version;
-    char* main_class;
-    char* class_path;
-    char* java_opts;
-    char* program_name;
     int is_jar;
-    char* jar_file;
+    char* program_name;
+
+    //    Maybe required
+    char* _main_class;
+    char* _jar_file;
+
+    //    Not required
+    char* _class_path;
+    char* _java_opts;
+
 };
 
 typedef struct s_props props;
 
+//Logic functions
 static int check_version(char* command);
 static void run_program(char* command, char *args[]);
 static props* read_props();
-static void free_props(props* props);
+static void free_props(props* p);
+static int check_props(props* p);
 
-char *strstrip(char *s);
-char* get_str_value(FILE* file, char* property);
+//Utility
+static char *strstrip(char *s);
+static char* get_str_value(FILE* file, char* property);
+static void error(char* error);
+static void p_error(char* error);
+static void safe_free(char* prop);
 
 int main(int argc, char* argv[]) {
     props * p = read_props();
-    int check = check_version("java");
-    if (check == 1) {
+    if (!p) {
+        error("Cannot create props");
+        exit(-1);
+    }
+    int valid = check_props(p);
+    if (!valid) {
+        error("Configuration file is not valid, check all options values");
+        exit(-1);
+    }
+    valid = check_version("java");
+    if (valid == 1) {
         //        printf("Java encontrado\n");
     }
     free_props(p);
@@ -41,42 +62,65 @@ static int check_version(char* command) {
     char* version_command = " -version 2>&1";
     memset(full_command, 0, 150);
     sprintf(full_command, "%s %s", command, version_command);
-    //    printf("Comando a ejecutar: %s\n", full_command);
     char output[1024];
     FILE* fp;
     fp = popen(full_command, "r");
     if (fp != NULL) {
-        //        puts("Comando ejecutado");
         if (fgets(output, sizeof (output), fp) != NULL) {
-            //            printf("Salida: %s", output);
         } else {
-            puts("No se pudo obtener la salida del comando\n");
+            puts("Cannot get output command\n");
         }
         pclose(fp);
         return 1;
     } else {
-        perror("Error al ejecutar comando");
+        perror("Error while executing command");
         return 0;
     }
 }
 
 static props* read_props() {
-    props* p = (props*) malloc(sizeof (props));
+    props* p = NULL;
     FILE* file = fopen("cdiff_test.cfg", "r");
     if (file != NULL) {
+        p = (props*) malloc(sizeof (props));
         char* version = get_str_value(file, "version");
-        char* main_class = get_str_value(file, "main_class");
-        char* class_path = get_str_value(file, "class_path");
-        char* java_opts = get_str_value(file, "java_opts");
-        char* program_name = get_str_value(file, "program_name");
         char* is_jar = get_str_value(file, "is_jar");
-        char* jar_file = get_str_value(file, "jar_file");
+        p->_main_class = get_str_value(file, "main_class");
+        p->_class_path = get_str_value(file, "class_path");
+        p->_java_opts = get_str_value(file, "java_opts");
+        p->program_name = get_str_value(file, "program_name");
+        p->_jar_file = get_str_value(file, "jar_file");
+        p->version = version != NULL ? atoi(version) : 0;
+        p->is_jar = is_jar != NULL ? atoi(is_jar) : 0;
         fclose(file);
     } else {
-        perror("No se pudo abrir el archivo de propiedades");
-        exit(-1);
+        p_error("Cannot open properties file");
     }
     return p;
+}
+
+static int check_props(props* p) {
+    if (p->version == 0) {
+        error("version option must be set");
+        return 0;
+    }
+    if (p->is_jar == 0) {
+        error("is_jar option must be set");
+        return 0;
+    }
+    if (p->is_jar) {
+        if (!p->_jar_file) {
+            error("jar_file option must be set");
+            return 0;
+        }
+    } else {
+        if (!p->_main_class) {
+            error("main_class option must be set");
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 char* get_str_value(FILE* file, char* property) {
@@ -103,9 +147,9 @@ char* get_str_value(FILE* file, char* property) {
     return value;
 }
 
-char *strstrip(char* s) {
+char* strstrip(char* s) {
     size_t size;
-    char *end;
+    char* end;
 
     size = strlen(s);
 
@@ -127,14 +171,28 @@ char *strstrip(char* s) {
 }
 
 static void free_props(props* p) {
-    //    free(p->main_class);
-    //    free(p->class_path);
-    //    free(p->java_opts);
-    //    free(p->program_name);
-    //    free(p->jar_file);
+    safe_free(p->_main_class);
+    safe_free(p->_class_path);
+    safe_free(p->_class_path);
+    safe_free(p->_java_opts);
+    safe_free(p->program_name);
     free(p);
+}
+
+static void safe_free(char* prop) {
+    if (prop) {
+        free(prop);
+    }
 }
 
 static void run_program(char* command, char *args[]) {
 
+}
+
+static void error(char* error) {
+    fprintf(stderr, "%s", error);
+}
+
+static void p_error(char* error) {
+    perror(error);
 }
